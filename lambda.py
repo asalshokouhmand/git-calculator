0c77b9a364442fb336f23f281226614333803d11
Asal Shokouhmand <asalshokouhmand@yahoo.com>
2025-12-10 02:24:07 +0330

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -2,6 +2,17 @@
 
 # print(x(32))
 # -----------------
-x = lambda a , b : a + b
+# x = lambda a , b : a + b
 
-print(x(10,11))
+# print(x(10,11))
+
+# ---------------------
+
+def myfunc(n):
+    # def new(a):
+    #     return a * n
+    # return new
+    return lambda a : a*n
+myfunc2 = myfunc(2)
+
+print(myfunc2(3))
